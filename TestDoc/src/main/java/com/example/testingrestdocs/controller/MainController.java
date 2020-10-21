64d9863d452d6c10a10ab6b196cd73c8f2eb4bf0
n.kasammongkonchai <n.kasammongkonchai@accenture.com>
2020-10-21 18:04:50 +0700

    complete setup fandamental

--- TestDoc/src/main/java/com/example/testingrestdocs/controller/MainController.java
+++ TestDoc/src/main/java/com/example/testingrestdocs/controller/MainController.java
@@ -1,4 +1,20 @@
 package com.example.testingrestdocs.controller;
 
+import com.example.testingrestdocs.TestingRestdocsApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
 public class MainController {
+
+    @GetMapping("/get")
+    private Mono<String> getDataInfo(){
+        return Mono.just("Hello world");
+    }
+
 }
